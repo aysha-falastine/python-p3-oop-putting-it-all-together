96320eab0aea58dbac0d11cd4a001ab2bf6724d0
asha mohamed <falastine924@gmail.com>
2025-08-25 18:28:37 +0300

    Use the property() function to create properties and validate input

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,63 @@
 #!/usr/bin/env python3
 
+# lib/book.py
+
 class Book:
-    pass
+    def __init__(self, title: str, page_count: int, author: str = None):
+        self._title = None
+        self._page_count = None
+        self._author = None
+
+        self.title = title
+        self.page_count = page_count
+        if author:
+            self.author = author
+
+    # ----- title -----
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("title must be a string")
+        self._title = value
+
+    # ----- page_count -----
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value: int):
+        # 👉 tests expect a *print* (not an exception) for non-integers
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            return
+        if value <= 0:
+            # you can keep this guard; tests don't check it, but it's fine
+            raise Exception("page_count must be > 0")
+        self._page_count = value
+
+
+    # ----- author -----
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("author must be a string")
+        self._author = value
+
+    # ----- instance method -----
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    def __repr__(self):
+        return f"Book(title={self.title!r}, pages={self.page_count}, author={self.author!r})"
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,59 @@
 #!/usr/bin/env python3
 
+# lib/shoe.py
+
 class Shoe:
-    pass
+    def __init__(self, brand: str, size: int):
+        self._brand = None
+        self._size = None
+        self._condition = "New"
+
+        self.brand = brand
+        self.size = size
+
+    # ----- brand -----
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("brand must be a string")
+        self._brand = value
+
+    # ----- size -----
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value: int):
+        
+        if not isinstance(value, int):
+            print("size must be an integer")
+            return
+        if value <= 0:
+            # optional extra guard; tests don't check this
+            raise Exception("size must be positive")
+        self._size = value
+
+
+    # ----- condition -----
+    @property
+    def condition(self):
+        return self._condition
+
+    @condition.setter
+    def condition(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("condition must be a string")
+        self._condition = value
+
+    # ----- instance method -----
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    def __repr__(self):
+        return f"Shoe(brand={self.brand!r}, size={self.size}, condition={self.condition!r})"
